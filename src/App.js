8cb75c3fcbe501a26beec2a95bd2f00b7e73cb32
zabiri <zachbiri@gmail.com>
2018-07-16 21:27:31 -0500

    Setting up animation for sidebar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
 import React, { Component } from "react";
-import Backdrop from './components/Backdrop/Backdrop'
+
+import Backdrop from "./components/Backdrop/Backdrop";
 import Toolbar from "./components/Toolbar/Toolbar";
-import SideDrawer from './components/SideDrawer/SideDrawer'
+import SideDrawer from "./components/SideDrawer/SideDrawer";
 import "./App.css";
 
 class App extends Component {
   state = {
     sideDrawerOpen: false
-  }
+  };
   backdropClickHandler = () => {
-    this.setState({sideDrawerOpen: false})
-  }
+    this.setState({ sideDrawerOpen: false });
+  };
   drawerToggleClickHandler = () => {
-    this.setState((prevState) => {
-      return {sideDrawerOpen: !prevState.sideDrawerOpen}
-    })
-  }
+    this.setState(prevState => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+    });
+  };
   render() {
-    let sideDrawer;
+
     let backdrop;
 
     if (this.state.sideDrawerOpen) {
-      sideDrawer=<SideDrawer/>
-      backdrop=<Backdrop click={this.backdropClickHandler}/>
+
+      backdrop = <Backdrop click={this.backdropClickHandler} />;
     }
     return (
       <div className="App">
-        <Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
-        {sideDrawer}
+        <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
+        <SideDrawer isOpen={this.state.sideDrawerOpen}/>
+        {/* {sideDrawer} */}
         {backdrop}
-        <main style={{marginTop: '64px'}}>
+        <main style={{ marginTop: "64px" }}>
           <p>This is the page content</p>
         </main>
       </div>
--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import './SideDrawer.css'
-const sideDrawer = () => {
+import React from "react";
+import { CSSTransition } from "react-transition-group";
+import "./SideDrawer.css";
+const sideDrawer = props => {
+    const animationTiming = {
+        enter: 400,
+        exit: 400
+    }
   return (
-    <nav className="SideDrawer">
-      <ul>
-          <li><a href="/">Products</a></li>
-          <li><a href="/">Users</a></li>
-      </ul>
-    </nav>
-  )
-}
+    <CSSTransition
+      mountOnEnter
+      unmountOnExit
+      timeout={animationTiming}
+      in={props.isOpen}
+      classNames="SideDrawer"
+    >
+      <nav className="SideDrawer">
+        <ul>
+          <li>
+            <a href="/">Products</a>
+          </li>
+          <li>
+            <a href="/">Users</a>
+          </li>
+        </ul>
+      </nav>
+    </CSSTransition>
+  );
+};
 
-export default sideDrawer
+export default sideDrawer;
