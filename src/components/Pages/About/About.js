523ee25e1bdfb008ef3bab90acfcc5858c3d46c2
Zach Biri <zachbiri@gmail.com>
2018-08-10 11:43:47 -0500

    Updating 'about' section

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,19 +1,35 @@
-import React from 'react'
-
-import './About.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./About.css";
 
 const about = () => {
   return (
     <section className="About">
       <div className="About__container">
         <h1 className="About__container-header">What is Yards & Gods?</h1>
-        <p className="About__container-paragraph">Yards & Gods was a mixed-media collective based in Columbia, MO from 2006-2013. We created a lot of beautiful things together, and I still consider Y&G folks family... But people come and go, and eventually that story came to an end.</p>
-        <p className="About__container-paragraph">Yards & Gods lives on in spirit here in this North Columbia home studio where many of the same voices (as well as plenty of new ones) can still be heard in the musicians that record here.</p>
+        <p className="About__container-paragraph">
+          Yards & Gods is a multimedia studio. While the primary focus is music
+          (recording, mixing, mastering), I also work on film audio, video
+          editing, animation, and 3D modeling.
+        </p>
+        <p className="About__container-paragraph">
+          Yards & Gods was also a mixed-media collective based in Columbia, MO
+          from 2006-2013. We released dozens of records, a zine, screen prints,
+          etc. Yards & Gods (the collective) still lives on in spirit, as many
+          of the same talents can be heard on the records recorded here.
+        </p>
         <h1 className="About__container-header">About the Engineer</h1>
-        <p className="About__container-paragraph">My name is Zach Biri. I am a/an: audio engineer, musician, audiophile, web developer, technophile, cat enthusiast, dabbler in mixed digital media, trekkie, etc... I operate a modest digital recording studio (sorry analog purists! someday...) out of my home in Columbia, MO for reasonable rates (inquire below!). I've been recording and engineering live sound for over half of my life.</p>
+        <p className="About__container-paragraph">
+          My name is Zach Biri. I am a/an: audio engineer, musician, web
+          developer, audiophile, technophile, cat enthusiast, video editor,
+          animator, trekkie, etc… I have been recording music and engineering
+          live sound for over half of my life. I operate a modest digital
+          recording studio in my home for reasonable rates (inquire
+          <NavLink to="./contact/">here</NavLink>).
+        </p>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default about
+export default about;
